127c4b551b8ca50cf9a0dbe2f4011f8756244c38
Suryan <suryanpinnoju@gmail.com>
2024-12-03 10:21:51 +0530

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 
 import { FaLinkedin } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa6";
 import { SiLeetcode } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
+import Sidebar from "./sidebar/Sidebar";
 
 const Navbar = () => {
   return (
     <div id="Home" className="flex items-center justify-between py-6">
+      <Sidebar />
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <a href="https://www.linkedin.com/in/suryan-pinnoju-7ba258288/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
             <a href="https://leetcode.com/u/suryanpinnoju/" target="_blank" rel="noopener noreferrer"><SiLeetcode /></a>
